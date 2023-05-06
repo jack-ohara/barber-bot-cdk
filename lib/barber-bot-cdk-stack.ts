0129ea370f0cc3cfaa4f619c96c116f91629c1bf
Jack O'Hara <jack.ohara@infinityworks.com>
2023-05-06 11:21:21 +0100

    dummy handler using layer

--- lib/barber-bot-cdk-stack.ts
+++ lib/barber-bot-cdk-stack.ts
@@ -1,16 +1,31 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as nodeJsLambda from "aws-cdk-lib/aws-lambda-nodejs";
+import { LayerVersion, Runtime as lambdaRuntime } from "aws-cdk-lib/aws-lambda";
 
 export class BarberBotCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'BarberBotCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pingBarberSiteLambda = new nodeJsLambda.NodejsFunction(
+      this,
+      "barber-bot-lambda",
+      {
+        runtime: lambdaRuntime.NODEJS_18_X,
+        entry: "./src/lambda/barber-bot-handler.ts",
+        functionName: "barber-bot",
+        bundling: {
+          sourceMap: true,
+          externalModules: ["aws-sdk", "aws-chrome-lambda-layer"],
+        },
+        layers: [
+          LayerVersion.fromLayerVersionArn(
+            this,
+            "AwsChromeLambdaLayer",
+            "arn:aws:lambda:eu-west-1:534699847887:layer:AwsChromeLambdaLayer:3"
+          ),
+        ],
+      }
+    );
   }
 }
